01c881872361fc402e3fd1fbe3efab6ea15a1491
Simon Farrow <simonwfarrow@gmail.com>
2021-09-28 17:29:56 +0100

    set timestamp, remove status, aged etc.

--- loadr/src/main/java/Loadr.java
+++ loadr/src/main/java/Loadr.java
@@ -1,6 +1,8 @@
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 public class Loadr implements Runnable {
@@ -9,6 +11,9 @@ public class Loadr implements Runnable {
     private final String topicName;
     private String json;
     private final int numEvents;
+
+    //2021-09-27T16:23:34.253Z
+    private static final SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
     Loadr(KafkaProducer<String, String> producer, String topicName, String json, int numEvents) {
         this.producer = producer;
@@ -26,8 +31,10 @@ public class Loadr implements Runnable {
             System.out.println("Thread id " + Thread.currentThread().getId() + " sending " + this.numEvents + " events");
             for (int i = 1; i <= numEvents; i++) {
 
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                String ts = sdf1.format(timestamp);
                 UUID id = UUID.randomUUID();
-                String payload = json.replaceAll("ID", id.toString());
+                String payload = json.replaceAll("ID", id.toString()).replaceAll("TIMESTAMP",ts);
                 producer.send(new ProducerRecord<String, String>(topicName, id.toString(), payload));
             }
 
